f044b0246a5fefdec5a90543df7170871e19d450
Wesley de Groot <email@wesleydegroot.nl>
2025-04-29 21:17:31 +0200

    Version 2.5 prep

---
+++ merge_to_25.php
@@ -0,0 +1,43 @@
+<?php
+
+// This script will generate the new data type for version 2.5
+
+$items = array(
+    'digging' => glob('./digging/*.json'),
+    'fishing' => glob('./fishing/*.json'),
+    'foraging' => glob('./foraging/*.json'),
+    'gardening' => glob('./gardening/*.json'),
+    'mining' => glob('./mining/*.json'),
+    'snippets' => glob('./snippets/*.json')
+);
+
+$resources = array();
+foreach ($items as $dir => $files) {
+    foreach ($files as $file) {
+        $contents = file_get_contents($file);
+        $json = json_decode($contents, true);
+
+        if (!isset($resources[$json['name']])) {
+            $resources[$json['name']] = $json;
+            $resources[$json['name']]['activity'] = array($dir);
+            unset($resources[$json['name']]['icon']);
+
+            if (isset($resources[$json['name']]['giftReward'])) {
+                $resources[$json['name']]['gift'] = $resources[$json['name']]['giftReward'];
+                unset($resources[$json['name']]['giftReward']);
+            }
+        } else {
+            if (!in_array($dir, $resources[$json['name']]['activity'])) {
+                $resources[$json['name']]['activity'][] = $dir;
+            }
+        }
+    }
+}
+
+foreach ($resources as $name => $value) {
+    $json = json_encode($value, JSON_PRETTY_PRINT);
+    file_put_contents("./resources/$name.json", $json);
+}
+
+print_r($resources);
+?>
