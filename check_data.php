9758ec9cf0f8a52f532f5858193ce683ff943e39
Wesley de Groot <email@wesleydegroot.nl>
2025-05-02 13:21:04 +0200

    Add more fish

---
+++ check_data.php
@@ -0,0 +1,49 @@
+<?php
+
+// This script merges all data files into one file
+$dirs = [
+    "biomes", 
+    "characters", 
+    "poi", 
+    "quests", 
+    "resources", 
+    "redeemables", 
+    "universes"
+];
+
+$requiredKeys = [
+    "biomes" => ["name", "description"],
+    "characters" => ["name", "description"],
+    "poi" => ["name", "description"],
+    "quests" => ["name", "description"],
+    "resources" => ["name", "description", "found"],
+    "redeemables" => ["name", "description"],
+    "universes" => ["name", "description"]
+];
+
+$errors = 0;
+foreach ($dirs as $directory) {
+    $files = glob($directory . "/*.json");
+
+    foreach ($files as $file) {
+        // Read the JSON file
+        $json = file_get_contents($file);
+        $json = json_decode($json, true);
+        if ($json === null) {
+            echo "Error decoding JSON from file: $file\n";
+            continue;
+        }
+
+        // Check for required keys
+        foreach ($requiredKeys[$directory] as $key) {
+            if (!isset($json[$key])) {
+                echo "Missing '$key' key in file: $file\n";
+                $errors++;
+            }
+        }
+    }
+}
+
+echo "Check completed.\n";
+echo "Total data errors found: $errors\n";
+?>
