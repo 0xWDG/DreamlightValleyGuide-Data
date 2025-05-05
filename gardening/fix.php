5149e08f48a0f4295c83026b5e942a5df8b5f2c6
Wesley de Groot <email@wesleydegroot.nl>
2025-05-05 10:43:02 +0200

    Fix gardening

---
+++ gardening/fix.php
@@ -0,0 +1,22 @@
+<?php
+
+$files = glob("*.json");
+
+foreach ($files as $file) {
+    $json = file_get_contents($file);
+    $json = json_decode($json, true);
+
+    if (isset($json['waterings'])) {
+        $json['waterings'] = intVal($json['waterings']);
+    }
+    
+    if (!isset($json['water'])) {
+        $json['water'] = $json['waterings'] ?? 0;
+    }
+
+    ksort($json);
+
+    file_put_contents($file, json_encode($json, JSON_PRETTY_PRINT));
+}
+
+?>
